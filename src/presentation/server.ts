b9084aabf8522f3ec0cb3e5cf1bd28d986762dd5
Duvan Smith <dsmith.mr@gmail.com>
2024-12-25 13:04:47 -0500

    .

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -7,16 +7,6 @@ export class Server {
     CronService.createJob(
         "*/5 * * * * *",
         ()=>{
-            const date = new Date();
-            console.log("first 5", date);
-        }
-    );
-
-    CronService.createJob(
-        "*/2 * * * * *",
-        ()=>{
-            const date = new Date();
-            console.log("first 2", date);
         }
     );
   }
