bf6d6134006396683d4cf1feb18c41e15b4af58d
Duvan Smith <dsmith.mr@gmail.com>
2024-12-25 12:52:57 -0500

    Added CronJob

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,16 @@
-export class Server{
-    public static start(){
-        console.log("Server started...");
-    }
+import { CronJob } from "cron";
+
+export class Server {
+  public static start() {
+    console.log("Server started...");
+
+    const job = new CronJob(
+      "*/2 * * * * *", // cronTime
+      () => {
+        console.log("You will see this message every second");
+      },
+    );
+
+    job.start();
+  }
 }
